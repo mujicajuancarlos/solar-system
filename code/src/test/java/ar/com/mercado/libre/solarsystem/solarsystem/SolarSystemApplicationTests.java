3a319316c2250760595778aa97bf556ecd166289
Juan Carlos Mujica <juan.mujica@gire.com>
2019-06-04 20:54:37 -0300

    Add spring boot starter

---
+++ code/src/test/java/ar/com/mercado/libre/solarsystem/solarsystem/SolarSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package ar.com.mercado.libre.solarsystem.solarsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SolarSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
